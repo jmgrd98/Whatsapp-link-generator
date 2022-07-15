e9e0a516a5ac0db65bce604a3f521fe52965902e
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-15 14:47:25 -0300

    js commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // Declarando as variáveis
 
-const input = document.getElementById('input');
+const number = document.getElementById("number").value;
+const msg = encodeURIComponent(document.getElementById("msg").value);
+const generateBtn = document.getElementByTagName("button");
+
+
+
+function generate() {
+    let link = "https://wa.me/"+number+"/?text="+msg;
+    let result = document.createElement("input");
+    result.value = link;
+    document.querySelector("form").appendChild(result);
+}
