4431e2e3c627257e7f039dcaabfb74614acd74ae
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-21 12:25:27 -0300

    copyBtn commit

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+let result = document.getElementById('result');
+let copyBtn = document.getElementById('copyBtn')
+
 function generate() {
 
     let number = document.getElementById("number").value;
     let msg = encodeURIComponent(document.getElementById("msg").value);
     let link = `https://wa.me/${number}/?text=${msg}`;
     let generateBtn = document.getElementById('generateBtn')
-    let result = document.getElementById('result');
-    let copyBtn = document.getElementById('copyBtn')
     result.style.visibility = 'visible';
     result.innerHTML = link;
     generateBtn.innerText = "Prontinho! ;)"
     copyBtn.style.visibility = 'visible';
     //document.querySelector("form").appendChild(result);
+}
+
+function copyText(){
+    copyBtn.innerText = 'Copiado!';
+    link.select();
+    link.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(link)
 }
