70abaa41f6610fc6f6a703c97bbffcaceded96cd
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-25 08:45:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let result = document.getElementById('result');
-let copyBtn = document.getElementById('copyBtn')
+//let copyBtn = document.getElementById('copyBtn')
 
 function generate() {
 
     let number = document.getElementById("number").value;
     let msg = encodeURIComponent(document.getElementById("msg").value);
-    let link = `https://wa.me/${number}/?text=${msg}`;
+    let link = `https://api.whatsapp.com/send?phone=${number}&text=${msg}`;
     let generateBtn = document.getElementById('generateBtn')
     result.style.visibility = 'visible';
     result.innerHTML = link;
     generateBtn.innerText = "Prontinho! ;)"
-    copyBtn.style.visibility = 'visible';
+    //copyBtn.style.visibility = 'visible';
     //document.querySelector("form").appendChild(result);
 }
 
-function copyText(){
-    copyBtn.innerText = 'Copiado!';
-    link.select();
-    link.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(link)
-}
+//function copyText(){
+    //copyBtn.innerText = 'Copiado!';
+    //link.select();
+    //link.setSelectionRange(0, 99999);
+    //navigator.clipboard.writeText(link)
+//}
